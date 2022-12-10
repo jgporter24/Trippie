2283a0550a9613717d82a18027a0d85d8524d383
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-12-10 15:02:02 -0600

    added code for base back end, integrating APIs next

---
+++ utils/helpers.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    fortmat_date: (date) => {
+        return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        return parseInt(amount).toLocaleString();
+    },
+    get_playlist: () => {
+        const
+    }
+}
