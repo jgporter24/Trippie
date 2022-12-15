b7c5f70c2067be74cdc942bd0c99f547f9cdcd81
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-12-14 20:03:13 -0600

    fixed connection bug and static folder bug

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,21 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
+    try {
+        
+        sequelize = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASSWORD,
+            {
+                host: '127.0.0.1',
+                dialect: 'mysql',
+                port: 3306
+            }
+        );
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
 module.exports = sequelize;
